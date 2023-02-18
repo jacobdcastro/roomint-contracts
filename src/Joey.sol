aab1b87d2e16339b7a6b641a860140ede17af8f0
Jacob D. Castro <jacob@jdc.dev>
2023-02-17 21:54:14 -0800

    basic scaffold for roomint

---
+++ src/Joey.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin/access/ownable.sol";
+import "openzeppelin/utils/Counters.sol";
+
+contract SoloMint is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function createNft(address _to, string memory _tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+
+        _tokenIds.increment();
+        return newTokenId;
+    }
+
+    function setTokenURI(uint256 _tokenId, string memory _tokenURI)
+        external
+        onlyOwner
+    {
+        _setTokenURI(_tokenId, _tokenURI);
+    }
+}
---
+++ src/Roo.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./Joey.sol";
+
+contract RooMint {
+    event ContractDeployed(address deployer, address contractAddress);
+
+    uint256 public etherDeployCost;
+    mapping(address => mapping(bool => uint256))
+        public supportedTokenDeployCost;
+    bool public deprecated;
+
+    constructor(uint256 _etherDeployCost) {
+        etherDeployCost = _etherDeployCost;
+    }
+
+    modifier verifyPayment(address tokenAddress) {
+        require(msg.value >= 0.1 ether, "Not enough ETH sent");
+        _;
+    }
+
+    function deployContract(
+        string memory _name,
+        string memory _symbol,
+        bool paidInEther
+    )
+        public
+        payable
+        verifyPayment(paidInEther ? address(0) : address(1))
+        returns (address)
+    {
+        SoloMint soloMint = new SoloMint(_name, _symbol);
+        return address(soloMint);
+    }
+}
